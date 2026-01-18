f302a5e7755de3f6f5c00fcfcd50a0cc269e0174
Eshaan <eshaanbhinder6@gmail.com>
2026-01-18 12:27:52 -0500

    added subystem layer to hopper and loader

--- src/main/java/frc/robot/subsystems/hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/hopper/Hopper.java
@@ -1,5 +1,36 @@
 package frc.robot.subsystems.hopper;
 
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Hopper extends SubsystemBase {}
+public class Hopper extends SubsystemBase {
+
+    private final HopperIO io;
+    private final HopperIOInputsAutoLogged inputs = new HopperIOInputsAutoLogged();
+
+    private final Debouncer motorConectedDebouncer = new Debouncer(0.5 ,DebounceType.kFalling);
+    private final Alert motorDisconnectedAlert = new Alert("Hopper motor disconnected!", Alert.AlertType.kWarning);
+
+
+    public Hopper(HopperIO io){
+        this.io = io;
+    }
+
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Hopper", inputs);
+
+        motorDisconnectedAlert.set(!motorConectedDebouncer.calculate(inputs.connected));
+    }
+
+    public Command runDutyCycleCommand(double percent){
+        return run(() -> io.runDutyCycle(percent));
+    }
+}
--- src/main/java/frc/robot/subsystems/loader/Loader.java
+++ src/main/java/frc/robot/subsystems/loader/Loader.java
@@ -1,5 +1,37 @@
 package frc.robot.subsystems.loader;
 
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Loader extends SubsystemBase {}
+public class Loader extends SubsystemBase{
+    
+    private final LoaderIO io;
+    private final LoaderIOInputsAutoLogged inputs = new LoaderIOInputsAutoLogged();
+
+    private final Debouncer motorConnectedDebouncer = new Debouncer(0.5, DebounceType.kFalling);
+    private final Alert loaderDisconnected = new Alert("Loader motor disconnected!", Alert.AlertType.kWarning);
+
+    public Loader(LoaderIO io){
+        this.io = io;        
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Loader", inputs);
+        loaderDisconnected.set(!motorConnectedDebouncer.calculate(inputs.connected));
+
+    }
+
+    public Command runDutyCycleCommand(double percent){
+        return run(() -> io.runDutyCycle(percent));
+    }
+
+    
+}
