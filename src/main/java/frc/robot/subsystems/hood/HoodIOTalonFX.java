7b7e9c28e318b437f19dacb2dfaeee8a517c0f93
Anderson <andersonbrodeur@icloud.com>
2026-01-20 14:19:34 -0500

    started optimizing CANBUS utilization

--- src/main/java/frc/robot/subsystems/hood/HoodIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIOTalonFX.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.hood;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
@@ -10,7 +12,13 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.Constants;
+import frc.robot.util.PhoenixUtil;
 
 public class HoodIOTalonFX implements HoodIO {
 
@@ -19,6 +27,15 @@ public class HoodIOTalonFX implements HoodIO {
 
   private final CANcoder encoder;
   private final CANcoderConfiguration encoderConfig;
+
+  private final StatusSignal<Temperature> temp;
+  private final StatusSignal<Angle> absolutePosition;
+  private final StatusSignal<Angle> rotorPosition;
+  private final StatusSignal<Double> positionSetpoint;
+  private final StatusSignal<AngularVelocity> velocity;
+  private final StatusSignal<Voltage> appliedVolts;
+  private final StatusSignal<Current> statorCurrent;
+  private final StatusSignal<Current> supplyCurrent;
 
   private final DutyCycleOut dutyCycle = new DutyCycleOut(0).withEnableFOC(true);
   private final PositionVoltage positionVoltage = new PositionVoltage(0).withEnableFOC(true);
@@ -54,23 +71,73 @@ public class HoodIOTalonFX implements HoodIO {
         Constants.Hood.ENCODER_DISCONTINUITY_POINT;
     encoderConfig.MagnetSensor.MagnetOffset = Constants.Hood.ENCODER_OFFSET;
 
-    hood.getConfigurator().apply(config);
-    encoder.getConfigurator().apply(encoderConfig);
+    PhoenixUtil.tryUntilOk(5, () -> hood.getConfigurator().apply(config, 0.25));
+    PhoenixUtil.tryUntilOk(5, () -> encoder.getConfigurator().apply(encoderConfig, 0.25));
+
+    temp = hood.getDeviceTemp();
+    absolutePosition = encoder.getAbsolutePosition();
+    rotorPosition = hood.getPosition();
+    positionSetpoint = hood.getClosedLoopReference();
+    velocity = hood.getVelocity();
+    appliedVolts = hood.getMotorVoltage();
+    statorCurrent = hood.getStatorCurrent();
+    supplyCurrent = hood.getSupplyCurrent();
+
+    BaseStatusSignal.setUpdateFrequencyForAll(
+      1 / Constants.loopTime,
+      temp,
+      absolutePosition,
+      rotorPosition,
+      positionSetpoint,
+      velocity,
+      appliedVolts,
+      statorCurrent,
+      supplyCurrent);
+    hood.optimizeBusUtilization();
+
+    PhoenixUtil.registerSignals(
+    false,
+    temp,
+    absolutePosition,
+    rotorPosition,
+    positionSetpoint,
+    velocity,
+    appliedVolts,
+    statorCurrent,
+    supplyCurrent);
   }
 
   @Override
   public void updateInputs(HoodIOInputs inputs) {
-    inputs.connected = hood.isConnected();
-    inputs.tempCelsius = hood.getDeviceTemp().getValueAsDouble();
+    BaseStatusSignal.refreshAll(
+      temp,
+      absolutePosition,
+      rotorPosition,
+      positionSetpoint,
+      velocity,
+      appliedVolts,
+      statorCurrent,
+      supplyCurrent);
+
+    inputs.connected = BaseStatusSignal.isAllGood(
+      temp,
+      absolutePosition,
+      rotorPosition,
+      positionSetpoint,
+      velocity,
+      appliedVolts,
+      statorCurrent,
+      supplyCurrent);
+    inputs.tempCelsius = temp.getValueAsDouble();
     inputs.absolutePositionRads =
-        Units.rotationsToRadians(encoder.getPosition().getValueAsDouble());
-    inputs.rotorPositionRads = Units.rotationsToRadians(hood.getPosition().getValueAsDouble());
+        Units.rotationsToRadians(absolutePosition.getValueAsDouble());
+    inputs.rotorPositionRads = Units.rotationsToRadians(rotorPosition.getValueAsDouble());
     inputs.positionSetpointRads =
-        Units.rotationsToRadians(hood.getClosedLoopReference().getValueAsDouble());
-    inputs.velocityRadsPerSec = Units.rotationsToRadians(hood.getVelocity().getValueAsDouble());
-    inputs.appliedVolts = hood.getMotorVoltage().getValueAsDouble();
-    inputs.statorCurrentAmps = hood.getStatorCurrent().getValueAsDouble();
-    inputs.supplyCurrentAmps = hood.getSupplyCurrent().getValueAsDouble();
+        Units.rotationsToRadians(positionSetpoint.getValueAsDouble());
+    inputs.velocityRadsPerSec = Units.rotationsToRadians(velocity.getValueAsDouble());
+    inputs.appliedVolts = appliedVolts.getValueAsDouble();
+    inputs.statorCurrentAmps = statorCurrent.getValueAsDouble();
+    inputs.supplyCurrentAmps = supplyCurrent.getValueAsDouble();
   }
 
   @Override
